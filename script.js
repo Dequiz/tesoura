f676822c5d4adec8da2388d58c7280d36c6be8cc
Dequiz <128110472+Dequiz@users.noreply.github.com>
2024-03-08 02:45:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function executar() {
+  jogar();
+  tesoura();
+}
+function executar2() {
+  jogar();
+  pedra();
+}
+function executar3() {
+  jogar();
+  papel();
+}
+function jogar() {
+  const resultado = random(1, 3);
+
+  if (resultado === 1) {
+    document.getElementById("jogo").innerHTML =
+      "<img src='img/papel-direita.png' class='jogadas'>";
+  } else if (resultado === 2) {
+    document.getElementById("jogo").innerHTML =
+      "<img src='img/pedra-direita.png' class='jogadas'>";
+  } else if (resultado === 3) {
+    document.getElementById("jogo").innerHTML =
+      "<img src='img/tesoura-direita.png' class='jogadas'>";
+  }
+}
+function tesoura() {
+  document.getElementById("jogo2").innerHTML =
+    "<img src='img/tesoura-esquerda.png' class='left'>";
+}
+function pedra() {
+  document.getElementById("jogo2").innerHTML =
+    "<img src='img/pedra-esquerda.png' class='left'>";
+}
+function papel() {
+  document.getElementById("jogo2").innerHTML =
+    "<img src='img/papel-esquerda.png' class='left'>";
+}
